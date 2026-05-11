0c3de91a5b2a62cdf2b4aa3f1c848db8b8d3045f
Sudharson PK <sudharsonp.k2005@gmail.com>
2026-05-11 14:41:15 +0530

    Time: 5 ms (65.15%), Space: 47.1 MB (27.8%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] asteroidCollision(int[] a) {
+        int l = a.length;
+        Stack<Integer> sb  = new Stack<>();
+        for(int i = 0;i<l;i++){
+            if(a[i]>0)sb.push(a[i]);
+            else{
+            while(!sb.isEmpty() &&  sb.peek() >0 && sb.peek()< Math.abs(a[i])){
+                sb.pop();
+            }
+            if( !sb.isEmpty() && sb.peek() == Math.abs(a[i])){
+                sb.pop();
+            }else if((sb.isEmpty() &&  a[i]<0) || sb.peek()<0){
+                sb.push(a[i]);
+            }
+            }
+        }
+        int s =  sb.size();
+        int[] b = new int[s];
+        for(int i = s-1;i>=0;i--){
+            b[i] = sb.peek();
+            sb.pop();
+        }
+        return b;
+    }
+}
